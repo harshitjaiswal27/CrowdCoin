95a4478889a895bde4f6679c733c38516a4e8937
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-27 01:01:11 +0530

    deployment script added

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new  HDWalletProvider(
+    '#PASTE YOUR ACCOUNT MNEMONIC SEED WORDS HERE#',
+    'https://rinkeby.infura.io/v3/#PASTE YOUR INFURA API KEY HERE#'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async ()=>{
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy contract from account : ' , accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data : compiledFactory.bytecode })
+    .send({ from : accounts[0] , gas : '10000000'})
+
+    console.log('Contract deployed at address :',result.options.address);
+};
+
+deploy();
+

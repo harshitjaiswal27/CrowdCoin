13f00c96527979c5ff996029a04d287e6096d94c
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-28 22:45:31 +0530

    Header added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout'
 
 class CamapignIndex extends Component {
     static async getInitialProps() {
@@ -21,11 +22,16 @@ class CamapignIndex extends Component {
     }
 
     render(){
-    return <div>
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-        {this.renderCampaigns()}
-        <Button content="Create Campaign" icon="add" secondary></Button>
-        </div>
+    return (
+        <Layout>
+            <div>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                <h3>OPEN CAMPAIGNS</h3>
+                {this.renderCampaigns()}
+                <Button content="Create Campaign" icon="add" primary></Button>
+            </div>
+        </Layout>
+        );
     }
 }
 

96708792fadb783946e26d9385b995f49ad0a15d
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-03-30 17:13:12 +0530

    component to add new request added

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { Button , Table} from 'semantic-ui-react';
+import {Link} from '../../../routes';
+import Layout from '../../../components/Layout';
+import RequestRow from '../../../components/RequestRow'
+import Campaign from '../../../ethereum/campaign'
+
+class RequestIndex extends Component{
+    static async getInitialProps(props){
+        const {address} = props.query;
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element,index)=>{
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        return {
+            address , requests, requestCount
+        };
+    }
+
+    renderRows(){
+        return this.props.requests.map((request,index)=>{
+            return (
+                <RequestRow
+                    key={Index}
+                    address={this.props.address}
+                    request={request}
+                />
+            )
+        })
+    }
+
+    render(){
+        const { Header, Row, HeaderCell, Body} = Table;
+        return (
+            <Layout>
+                <h3>Requests</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                    <a>
+                        <Button icon="add" content="Add Request" floated="right" primary></Button>
+                    </a>
+                </Link>
+                <Table> 
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
+                </Table>
+            </Layout>
+        );
+    };
+}
+
+export default RequestIndex;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Grid, Card} from 'semantic-ui-react';
+import { Grid, Card, Button} from 'semantic-ui-react';
 import web3 from '../../ethereum/web3'
 import Layout from '../../components/Layout';
 import ContributeForm from '../../components/ContributeForm'
 import Campaign from '../../ethereum/campaign';
+import { Link } from '../../routes'
 
 class CampaignShow extends Component {
     static async getInitialProps(props){
@@ -56,14 +57,25 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign Details</h3>
                 <Grid>
-                    <Grid.Column width={10}>
-                        {this.renderCards()}
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <h3>Contribute to this Campaign</h3>
-                        <ContributeForm address={this.props.address}/>
-                    </Grid.Column>   
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCards()}
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <h3>Contribute to this Campaign</h3>
+                            <ContributeForm address={this.props.address}/>
+                        </Grid.Column>  
+                    </Grid.Row> 
+
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={`/campaigns/${this.props.address}/requests`}>
+                                <a><Button primary>View Requests</Button></a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                  </Grid>
+                 
             </Layout>
         );
     }
